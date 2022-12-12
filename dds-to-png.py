dde5a160aec6d455497686856252e3059c934a49
Gabriella Agathon <gjstimac@gmail.com>
2022-12-12 16:37:43 -0600

    Created file for dds-to-png

---
+++ dds-to-png.py
@@ -0,0 +1,15 @@
+from wand import image
+import os.path
+
+"""
+with image.Image(filename="white_rect_dxt3.dds") as img:
+    img.compression = "no"
+    img.save(filename="white_rect_dxt3.png")
+"""
+
+"""
+This is a script that takes a file folder that contains .dds 
+files and then outputs them to a different file folder as .png's
+
+"""
+
